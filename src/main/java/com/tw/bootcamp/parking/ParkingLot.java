baa01b0639ceec21052c477d581d52ca90b3ac15
mengjun <mdun123@ap.xom.com>
2018-12-24 16:35:24 +0800

    mengjun: add the implement that parkinglot has empty space

---
+++ src/main/java/com/tw/bootcamp/parking/ParkingLot.java
@@ -0,0 +1,12 @@
+package com.tw.bootcamp.parking;
+
+public class ParkingLot {
+
+  public ParkingLot(int space) {
+
+  }
+
+  public ParkingTicket in(Car car) {
+    return new ParkingTicket();
+  }
+}
