7697b0b4b207d90cd701136a669d1c481574df31
mengjun <mdun123@ap.xom.com>
2018-12-24 16:49:52 +0800

    mengjun: add unit test that parkingLot only has my car, then i can pick my car by paring ticket

--- src/test/java/com/tw/bootcamp/parking/ParkingLotTest.java
+++ src/test/java/com/tw/bootcamp/parking/ParkingLotTest.java
@@ -2,6 +2,7 @@ package com.tw.bootcamp.parking;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -17,10 +18,21 @@ class ParkingLotTest {
 
   @Test
   void shouldThrowParkingLotIsFullExceptionWhenParkACarGivenParkingLotHasNoEmptySpace() {
-    ParkingLot parkingLot = new ParkingLot(1);
-    parkingLot.in(new Car());
+    ParkingLot parkingLot = new ParkingLot(0);
 
     assertThrows(ParkingLotIsFullException.class, () -> parkingLot.in(new Car()));
+  }
+
+
+  @Test
+  void shouldReturnParkedCarWhenPickCarGivenParkingLotOnlyHasMyCar() {
+    ParkingLot parkingLot = new ParkingLot(1);
+    Car myCar = new Car();
+    ParkingTicket ticket = parkingLot.in(myCar);
+
+    Car pickedCar = parkingLot.out(ticket);
+
+    assertEquals(myCar, pickedCar);
   }
 
 }
