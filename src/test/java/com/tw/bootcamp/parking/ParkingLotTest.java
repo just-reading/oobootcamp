978479a0866476ab65afd25894ae5936cc442317
mengjun <mdun123@ap.xom.com>
2018-12-24 16:33:04 +0800

    mengjun: add test that parkinglot has empty space

--- src/test/java/com/tw/bootcamp/parking/ParkingLotTest.java
+++ src/test/java/com/tw/bootcamp/parking/ParkingLotTest.java
@@ -1,5 +1,16 @@
 package com.tw.bootcamp.parking;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class ParkingLotTest {
 
+  @Test
+  void shouldReturnTicketWhenParkACarGivenParkingLotHasOneEmptySpace() {
+    ParkingLot parkingLot = new ParkingLot(1);
+    ParkingTicket ticket = parkingLot.in(new Car());
+
+    assertNotNull(ticket);
+  }
 }
