f929e5fb19be5dc5fa27b620bbb39b41e5b0aa21
mengjun <mdun123@ap.xom.com>
2019-01-02 14:56:16 +0800

    mengjun: first task

--- src/test/java/com/tw/bootcamp/parking/SmartParkingBoyTest.java
+++ src/test/java/com/tw/bootcamp/parking/SmartParkingBoyTest.java
@@ -1,5 +1,25 @@
 package com.tw.bootcamp.parking;
 
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 public class SmartParkingBoyTest {
 
+  @Test
+  void shouldReturnTicketWhenParkACarGivenSmartParkingBoyHasTwoParkingLotAndTheyHaveTheSameParkingSpace() {
+    ParkingLot firstParkingLot = new ParkingLot(1);
+    ParkingLot secondParkingLot = new ParkingLot(1);
+
+    ParkingBoy parkingBoy = new SmartParkingBoy(Arrays.asList(firstParkingLot, secondParkingLot));
+
+    Car myCar = new Car();
+
+    ParkingTicket parkingTicket = parkingBoy.park(myCar);
+
+    assertNotNull(parkingTicket);
+    assertSame(myCar, firstParkingLot.pick(parkingTicket));
+  }
 }
